fce2440c77635e98a081622e428f1b0e891e64a4
朱新伟 <zhuxinwei@ifugle.com>
2019-06-14 15:13:30 +0800

    'everyday.md排版方式修改'

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -14,17 +14,20 @@
             }
         },
         created() {
-            const arr1 = [3,5,5]
-            const arr2 = [6,7,8]
-            const arr3 = [[9,10,11],12]
-            const arr = arr1.concat(arr2,arr3)
-            arr[6][0] = 10
-
-            console.log(arr1) //[3,5,5]
-            console.log(arr2) //[6,7,8]
-            console.log(arr3) //[[10,10,11],12]
-            arr3[0][0] = 9
-            console.log(arr) // [3,5,5,6,7,8,[9,10,11],12]
+            let obj = {
+                name: 'lilei',
+                age: 28,
+                pingjia:{
+                    good: 25,
+                    bad: 44
+                }
+            }
+            let copy = Object.assign({},obj)
+            //这个时候会拷贝第一层的值，但是对象里面的对象是浅拷贝，拷贝的是引用，测试一下
+            copy.name = 'laowang'
+            copy.age = 28
+            copy.pingjia.good = 55
+            console.log(obj)
         },
         mounted() {
 
