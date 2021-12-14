7a68768d568a6443a81a2ce55f669f0b88e2b4dc
Michaela <995037977@qq.com>
2021-12-14 18:12:23 +0800

    feat:推送本地到github测试

--- src/api/login.js
+++ src/api/login.js
@@ -1,3 +1,4 @@
+// 测试提交远程github
 import request from '@/utils/service'
 
 export function getToken(param, cancelToken) {
@@ -12,6 +13,13 @@ export function verifyToken(param, cancelToken) {
   return request({
     method: 'post',
     url: '/v1/token/verify',
-    data: param,
+    data: param
+  })
+}
+export function test(param, cancelToken) {
+  return request({
+    method: 'post',
+    url: '/v1/token/verify',
+    data: param
   })
 }
