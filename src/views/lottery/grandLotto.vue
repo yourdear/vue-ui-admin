734e6f6f206d2b03886b4c6871c2533947ba3c20
Michaela <995037977@qq.com>
2021-12-17 08:37:41 +0800

    test:github触发流水线测试

--- src/views/lottery/grandLotto.vue
+++ src/views/lottery/grandLotto.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <el-button @click="getLottoNumber">随机</el-button>
-    <two :testData="testData"></two>
+    <el-button @click="getLottoNumber">乐透随机</el-button>
+    <el-button @click="getshuangseqiuNumber">双色球随机</el-button>
+    <div>{{ lotto[0] }}-{{ lotto[1] }}</div>
+    <two :test-data="testData" />
   </div>
 </template>
 <script>
@@ -13,8 +15,9 @@ export default {
   },
   data() {
     return {
+      lotto: [[], []],
       testData: {
-        name: 'zs',
+        name: 'zs'
       }
     }
   },
@@ -33,20 +36,40 @@ export default {
       const backAra = lotto[1]
       for (let i = 0; i < 7; i++) {
         if (frontAra.length < 5) {
-          const number = Math.ceil(Math.random()*35)
+          const number = Math.ceil(Math.random() * 35)
           frontAra.push(number)
         } else {
-          const number = Math.ceil(Math.random()*12)
+          const number = Math.ceil(Math.random() * 12)
           backAra.push(number)
         }
       }
       const frontAraLength = Array.from(new Set(frontAra)).length
       const backAraLength = Array.from(new Set(backAra)).length
-      if(frontAraLength === 5 && backAraLength === 2) {
-        console.log(lotto[0])
-        console.log(lotto[1])
+      if (frontAraLength === 5 && backAraLength === 2) {
+        this.lotto = lotto
       } else {
         this.getLottoNumber()
+      }
+    },
+    getshuangseqiuNumber() {
+      const lotto = [[], []]
+      const frontAra = lotto[0]
+      const backAra = lotto[1]
+      for (let i = 0; i < 7; i++) {
+        if (frontAra.length < 6) {
+          const number = Math.ceil(Math.random() * 33)
+          frontAra.push(number)
+        } else {
+          const number = Math.ceil(Math.random() * 16)
+          backAra.push(number)
+        }
+      }
+      const frontAraLength = Array.from(new Set(frontAra)).length
+      const backAraLength = Array.from(new Set(backAra)).length
+      if (frontAraLength === 6 && backAraLength === 1) {
+        this.lotto = lotto
+      } else {
+        this.getshuangseqiuNumber()
       }
     }
   }
