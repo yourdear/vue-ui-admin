7285476210f9ae9131660fd7208eb3cb31bdc319
Michaela <995037977@qq.com>
2021-12-20 10:33:45 +0800

    test:流程测试

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <div class="login-box">
-      <h2 class="login-title">登录修改后</h2>
+      <h2 class="login-title">登录修改后?</h2>
       <el-form :model="formInline">
         <el-form-item>
           <el-input v-model="formInline.user"
